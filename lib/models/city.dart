4aa85f5b98382de9119f4efe0d99f357d6eaaccf
Alexander Melara Avalos <lexmelara@gmail.com>
2019-01-29 15:28:02 -0600

    change city_service & model

--- lib/models/city.dart
+++ lib/models/city.dart
@@ -5,11 +5,18 @@ class City {
   final Coordinate coordinate;
 
   const City({this.id, this.name, this.country, this.coordinate});
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      id: json['id'],
+      name: json['name'],
+      coordinate: json['coord']
+    );
+  }
 }
 
 class Coordinate {
   final double lon;
   final double lat;
-
   const Coordinate(this.lon, this.lat);
 }
--- lib/services/city_service.dart
+++ lib/services/city_service.dart
@@ -1,24 +1,21 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:weather_app/models/city.dart';
 
-Future<http.Response> getCities(String city) async {
+Future<List<City>> getCities(String city) async {
 
   try{
     var uri = Uri.parse('http://localhost:8080/api/city/get-city-by-name');
     uri = uri.replace(query: 'name=' + city);
     final response = await http.get(uri);
-    return json.decode(response.body);
+    final listCities = json.decode(response.body);
+    List<City> cities = [];
+    if(response.body != null){
+      cities = listCities.map((model) => City.fromJson(listCities)).toList();
+    }
+    return cities;
   }
   catch(exception){
     return null;
   }
-//  try {
-//    return await http.get('').then((dynamic res) {
-//      return json.decode(res.body);
-//    }).catchError((e) {
-//      throw Exception('Failed');
-//    });
-//  } catch (exception) {
-//
-//  }
 }
